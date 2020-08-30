a6db864728526be3efa803ebc4bf9cbb830c86af
johnjqc <johnquirogac@gmail.com>
2020-08-30 18:43:48 -0500

    modify blog format

--- _site/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "You're up and running!",
-        "excerpt":"Next you can update your site name, avatar and other options using the _config.yml file in the root of your repository (shown below). The easiest way to make your first post is to edit this one. Go into /_posts/ and update the Hello World markdown file. For more instructions head...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/Hello-World/",
-        "teaser": null
-      }]
