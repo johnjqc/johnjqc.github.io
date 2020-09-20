b307bf26232617bff383c5a5003af12b05325cea
johnjqc <johnquirogac@gmail.com>
2020-09-20 17:06:39 -0500

    change to reacta app

---
+++ precache-manifest.899c0c03bbbdf9f14d49c5fc28196eab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680b6dadabaca5d993aeea62aa440d5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d044585fd48b85e4c7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "77c1d3dd973b4f656ade",
+    "url": "/static/js/2.d394c2df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d394c2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d044585fd48b85e4c7",
+    "url": "/static/js/main.b0992395.chunk.js"
+  },
+  {
+    "revision": "1e1a2dd871eac600b8b9",
+    "url": "/static/js/runtime-main.435b2331.js"
+  },
+  {
+    "revision": "27b56736bf45043f56471edc80850503",
+    "url": "/static/media/yo32.27b56736.jpeg"
+  }
+]);
